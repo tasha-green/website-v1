4d74d7a0d2c9192b2428086a88c8ef89ef811a3f
Natasha Green <natasha2green@gmail.com>
2023-04-28 16:23:37 -0700

    basic setup

---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+  Hey guys! I have a website!!  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+}
+</style>
