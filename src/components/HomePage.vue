843d4c29f1aff6a0c2b279203713a5aac0c4bc29
Natasha Green <natasha2green@gmail.com>
2023-04-30 14:40:14 -0700

    navbar to app.vue to avoid repetition for each pg

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <!--<nav>
     <div class="menuBar">
       <div class="logo">
         <p id="firstName">{{ logoFirst }}</p>
@@ -20,7 +20,7 @@
         </li>
       </ul>
     </div>
-  </nav>
+  </nav>-->
 
   <div class="textToTest">
     <p>
@@ -55,115 +55,12 @@
 <script>
 export default {
   name: 'HomePage',
-  data() {
-    return {
-      logoFirst: "Natasha",
-      logoLast: "Green",
-      aboutLink: "ABOUT",
-      workDropdown: "WORK",
-      dropDownOptionDesign: "Design",
-      dropDownOptionJS: "Javascript Projects",
-      dropDownOptionModel: "Modelling",
-      dropDownOptionUnity: "Unity"
-
-    }
-
-  }
-}
-
-let nav = document.querySelector("nav");
-window.onscroll = function() {
-  if(document.documentElement.scrollTop > 30) {
-    nav.classList.add("sticky");
-  }
-  else {
-    nav.classList.remove("sticky");
-  }
+  
 }
 
 </script>
 
 <style scoped>
-
-* {
-  text-decoration: none;
-}
-
-nav.sticky {
-  background: #00A4BD;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
-}
-.menuBar {
-  background-color: #A9D18E;
-  padding-left: 30px;
-  padding-right: 50px;
-  display: flex;
-  justify-content: space-between;
-}
-
-.navItem {
-  background-color: transparent;
-  border: transparent;
-  color:#000000;
-  font-weight: 200;
-  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  font-size: 15px;
-}
-
-
-.logo {
-  display: flex;
-  font-size: 25px;
-  font-weight: 500;
-  font-family: 'Courier New', Courier, monospace;
-}
-
-.navBar {
-  display: flex;
-  align-items: center;
-}
-
-ul {
-  list-style-type:none;
-}
-
-
-#firstName {
-  padding-right: 10px;
-}
-
-.dropdown {
-  padding-left: 20px;
-  display: inline-block;
-  position: relative;
-}
-
-.dropdownContent {
-  display: none;
-  background-color: #FFFFFF;
-  position: absolute;
-  width: 100%;
-  overflow: auto;
-  box-shadow: 0px 10px 10px 0px rgba(0, 0, 0, 0.1);
-}
-
-.dropdown:hover .dropdownContent {
-  display:list-item ;
-}
-
-.dropdownContent a {
-  display: block;
-  color: #000000;
-  padding: 5px;
-  font-weight: 200;
-  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  font-size: 15px;
-}
-
-.dropdownContent a:hover {
-  color: #FFFFFF;
-  background-color: #00A4BD;
-}
 
 .textToTest {
   text-align: justify;
