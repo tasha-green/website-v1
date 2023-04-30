9f3c7d3b95948d0d946f0922078c7d4d7dba96b0
Natasha Green <natasha2green@gmail.com>
2023-04-29 17:31:21 -0700

    Working on the menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <HomePage msg="Welcome to Your Vue.js App"/>
+  <HomePage/>
 </template>
 
 <script>
@@ -12,14 +12,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,23 +1,64 @@
 <template>
-  <div class="hello">
-  Hey guys! I have a website!!  
+  <div class="menuBar">
+    <div class="logo">
+      <p id="firstName">{{ logoFirst }}</p>
+      <p id="lastName">{{ logoLast }}</p>
+    </div>
+    <ul class="navBar">
+      <li><a class="navItem" href="#">{{ aboutLink }}</a></li>
+      <li><a class="navItem" href="#">{{ workDropdown }}</a></li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
-  props: {
-    msg: String
+  data() {
+    return {
+      logoFirst: "Natasha",
+      logoLast: "Green",
+      aboutLink: "ABOUT",
+      workDropdown: "WORK"
+    }
+
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello {
-  text-align: center;
+.menuBar {
+  background-color: #A9D18E;
+  padding-left: 2%;
+  padding-right: 2%;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
 }
+.logo {
+  display: flex;
+  font-size: 25px;
+  font-weight: 500;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+.navBar {
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.navItem {
+  padding-left: 10px;
+}
+
+ul {
+  list-style-type:none;
+}
+
+
+#firstName {
+  padding-right: 10px;
+}
+
 </style>
